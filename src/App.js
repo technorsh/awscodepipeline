b12de9b92694f290fb9b848004740f0379a84c26
technorsh <technorsh@gmail.com>
2022-02-23 19:41:54 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Pipeline created by Rahul Sharma
+          Pipeline created by ~ Rahul Sharma
         </p>
       </header>
     </div>
