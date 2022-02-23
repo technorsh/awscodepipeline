934fc140677964c9acc99d8d3aa9655e215e1e8c
technorsh <technorsh@gmail.com>
2022-02-23 16:55:43 +0530

    appspec.yml updated

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_17.x | sudo bash -
 yum -y install nodejs npm --skip-broken
 npm install pm2 -g
 # curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
