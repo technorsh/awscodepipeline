5481e8d941cf74b33b0f3866415f8fc9146e1aeb
technorsh <technorsh@gmail.com>
2022-02-23 17:57:33 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Pipeline created by Rahul Sharma
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
