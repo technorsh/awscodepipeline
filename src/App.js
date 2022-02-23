e83b1b42ba489545f79af1d2d75bc2733fdf9fa4
technorsh <technorsh@gmail.com>
2022-02-23 18:37:49 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ function App() {
         <p>
           Pipeline created by Rahul Sharma
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by ~ Rahul Sharma :)
-        </a>
       </header>
     </div>
   );
